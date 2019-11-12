58aaac6adb5f7dcced5ac5ee0e620b2d5f279183
Vidyasagar <vidyasagar.ananthan@gmail.com>
2019-11-12 18:50:10 -0500

    more CORS issues.

--- animations.js
+++ animations.js
@@ -1,7 +1,3 @@
-/* import * as THREE from "./node_modules/three/build/three.module.js";
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { STLLoader } from './node_modules/three/examples/jsm/loaders/STLLoader.js';*/
-THREE.Cache.enabled = true;
 var scene, renderer, camera, controls, light, textMesh, textGeo, geometry, material, t, stats, scale, path3, geometry3,
   material3, path4, geometry4, mesh3, mesh4, path5, path6, mesh5, mesh6, geometry5, geometry6, material5, path7, path8, mesh8, mesh7, geometry7, geometry8, material6, material9, mesh9;
 t = 0; var loader = new THREE.FontLoader(); scale = 0; var lod = new THREE.LOD(); lod.update = false; var stlloader = new THREE.STLLoader();
@@ -29,13 +25,12 @@ function init() {
   var text = "Vidyasagar, Ph.D.",
     height = .05,
     size = 5,
-    hover = 10,
     curveSegments = 10,
     bevelThickness = .1,
     bevelSize = .15,
     bevelEnabled = true; // normal bold
 
-  loader.load('https://github.com/v10z/v10z.github.io/tree/master/node_modules/three/examples/fonts/optimer_regular.typeface.json', function (font) {
+  loader.load('./node_modules/three/examples/fonts/optimer_regular.typeface.json', function (font) {
     textGeo = new THREE.TextGeometry(text, {
       font: font,
       size: size,
@@ -204,7 +199,7 @@ function init() {
   scene.add(mesh7);
 
 
-  stlloader.load('https://github.com/v10z/v10z.github.io/tree/master/stl/spin.stl', function (geometry9) {
+  stlloader.load('./stl/spin.stl', function (geometry9) {
     var material9 = new THREE.MeshPhysicalMaterial({ reflectivity: 0.1, clearcoatRoughness: 0.1, color: "rgb(255,215,0)" });
     var mesh9 = new THREE.Mesh(geometry9, material9);
     mesh9.position.set(28, 15, -30);
@@ -355,9 +350,9 @@ function init() {
         shininess: 200
       },
       textures: {
-        map: 'https://github.com/v10z/v10z.github.io/tree/master/images/earthmap1k.jpg',
-        bumpMap: 'https://github.com/v10z/v10z.github.io/tree/master/images/earthbump1k.jpg',
-        specularMap: 'https://github.com/v10z/v10z.github.io/tree/master/images/earthspec1k.jpg'
+        map: './images/earthmap1k.jpg',
+        bumpMap: './images/earthbump1k.jpg',
+        specularMap: './images/earthspec1k.jpg'
       }
     },
     atmosphere: {
@@ -366,8 +361,8 @@ function init() {
         opacity: 0.9
       },
       textures: {
-        map: 'https://github.com/v10z/v10z.github.io/tree/master/images/earthcloudmap.jpg',
-        alphaMap: 'https://github.com/v10z/v10z.github.io/tree/master/images/earthcloudmaptrans.jpg'
+        map: './images/earthcloudmap.jpg',
+        alphaMap: './images/earthcloudmaptrans.jpg'
       },
       glow: {
         size: 0.1,
@@ -379,23 +374,6 @@ function init() {
   });
 
   scene.add(earth);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   animate();
 }
 
